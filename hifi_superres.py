9cf41e7c2dd390e31f74bffb05e1564579319576
Rainbow Llama <rainbowllama123@airmail.cc>
2022-08-31 07:50:25 -0700

    move superres functionality to a function

--- hifi_superres.py
+++ hifi_superres.py
@@ -8,6 +8,13 @@ from meldataset import MAX_WAV_VALUE
 DEVICE = "cuda:0"
 RUN_PATH = os.path.dirname(os.path.realpath(__file__))
 
+def superres(audio, sr_voc, scale=MAX_WAV_VALUE):
+        audio = audio * scale
+        assert (samplerate == 22050), 'input must be 22.05kHz'
+
+        sr_mix, new_rate = sr_voc.superres(audio, 22050)
+        return sr_mix, new_rate
+
 if __name__ == "__main__":
     sr_voc = vocoder.HiFiGAN(
         os.path.join(RUN_PATH, "models", "hifisr"),
@@ -18,10 +25,7 @@ if __name__ == "__main__":
 
     for n in args.name:
         audio, samplerate = sf.read(n)
-        audio = audio * MAX_WAV_VALUE
-        assert (samplerate == 22050), 'input must be 22.05kHz'
-
-        sr_mix, new_rate = sr_voc.superres(audio, 22050)
+        sr_mix, new_rate = superres(audio, sr_voc)
 
         sf.write(os.path.splitext(os.path.basename(n))[0] + '_sr.wav',
             sr_mix.astype(np.int16), new_rate)
