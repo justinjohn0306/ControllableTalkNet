ccd3c57d3236df949ec19b347dcfdf5e5b7c17b3
Rainbow Llama <rainbowllama123@airmail.cc>
2022-08-31 07:52:20 -0700

    no vocoder as param

--- hifi_superres.py
+++ hifi_superres.py
@@ -7,8 +7,11 @@ from meldataset import MAX_WAV_VALUE
 
 DEVICE = "cuda:0"
 RUN_PATH = os.path.dirname(os.path.realpath(__file__))
+sr_voc = vocoder.HiFiGAN(
+    os.path.join(RUN_PATH, "models", "hifisr"),
+    "config_32k", DEVICE)
 
-def superres(audio, sr_voc, scale=MAX_WAV_VALUE):
+def superres(audio, scale=MAX_WAV_VALUE):
         audio = audio * scale
         assert (samplerate == 22050), 'input must be 22.05kHz'
 
@@ -16,16 +19,13 @@ def superres(audio, sr_voc, scale=MAX_WAV_VALUE):
         return sr_mix, new_rate
 
 if __name__ == "__main__":
-    sr_voc = vocoder.HiFiGAN(
-        os.path.join(RUN_PATH, "models", "hifisr"),
-        "config_32k", DEVICE)
     parser = argparse.ArgumentParser()
     parser.add_argument('name', nargs='*')
     args = parser.parse_args()
 
     for n in args.name:
         audio, samplerate = sf.read(n)
-        sr_mix, new_rate = superres(audio, sr_voc)
+        sr_mix, new_rate = superres(audio)
 
         sf.write(os.path.splitext(os.path.basename(n))[0] + '_sr.wav',
             sr_mix.astype(np.int16), new_rate)
