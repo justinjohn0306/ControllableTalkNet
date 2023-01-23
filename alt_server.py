44d5b314fb1c1950a42c6910b5b241d74cb8e8a1
Baked Pineapple <bakedpineapple@airmail.cc>
2023-01-23 13:14:12 -0800

    add alt server

---
+++ alt_server.py
@@ -0,0 +1,197 @@
+import os
+import base64
+import torch
+import numpy as np
+import tensorflow as tf
+from scipy.io import wavfile
+import io
+from nemo.collections.tts.models import TalkNetSpectModel
+from nemo.collections.tts.models import TalkNetPitchModel
+from nemo.collections.tts.models import TalkNetDursModel
+from core.talknet_singer import TalkNetSingerModel
+from core import extract, vocoder, reconstruct
+from core.download import download_from_drive, download_reconst
+import os
+import json
+import ffmpeg
+import traceback
+import librosa
+import soundfile
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+DEVICE = "cuda:0"
+RUN_PATH = os.path.dirname(os.path.realpath(__file__))
+
+# Assumes ref audio, no metallic noise reduction, no autotune
+class AltTalknetServer:
+    def __init__(self):
+        self.extract_dur = extract.ExtractDuration(RUN_PATH, DEVICE)
+        self.extract_pitch = extract.ExtractPitch()
+
+        self.model_mapping = {}
+        self.wav_f0s = {}
+        self.tnmodel = None
+        self.tnpath = ""
+        self.tndurs = None
+        self.tnpitch = None
+        self.voc = None
+        self.last_voc = ""
+        self.sr_voc = None
+        self.init_model_mapping()
+        self.output_mels = True
+
+        #self.from_file0("test_file.txt")
+
+        pass
+
+    def from_file0(self, fil):
+        wav_list = []
+        lines = []
+        with open(fil) as f:
+            for line in f.readlines():
+                ls = line.split('|')
+                print(len(ls))
+                wav_list.append(ls[0])
+                lines.append(ls)
+        for w in wav_list:
+            self.preprocess_wav(w)
+        for l in lines:
+            gen = self.generate_audio(l[1],l[0],l[2])
+            with(open(gen[2],"wb")) as f:
+                f.write(gen[0])
+
+    # Build model mapping from list
+    def init_model_mapping(self):
+        for filename in os.listdir("model_lists"):
+            if len(filename) < 5 or filename[-5:].lower() != ".json":
+                continue
+            with open(os.path.join("model_lists", filename)) as f:
+                j = json.load(f)
+                for source in j:
+                    self.map_source(source)
+        pass
+
+    def map_source(self, source):
+        for char in source["characters"]:
+            self.model_mapping[char["name"]] = char["drive_id"]
+        pass
+
+    # Pitch analysis
+    def preprocess_wav(self, wav_path):
+        if not os.path.exists(os.path.join(RUN_PATH, "temp")):
+            os.mkdir(os.path.join(RUN_PATH, "temp"))
+        ffmpeg.input(os.path.join(RUN_PATH, wav_path)).output(
+            os.path.join(RUN_PATH, "temp", wav_path + "_conv.wav"),
+            ar="22050",
+            ac="1",
+            acodec="pcm_s16le",
+            map_metadata="-1",
+            fflags="+bitexact",
+        ).overwrite_output().run(quiet=True)
+        f0_with_silence, f0_wo_silence = self.extract_pitch.get_pitch(
+            os.path.join(RUN_PATH, "temp", wav_path + "_conv.wav"),
+            legacy=True,
+        )
+        self.wav_f0s[wav_path] = {
+            "f0_with_silence": f0_with_silence, 
+            "f0_wo_silence": f0_wo_silence}
+        pass
+
+    # Audio generation
+    def generate_audio(self, char, wav_name, transcript):
+        if transcript is None or transcript.strip() == "":
+            raise ValueError("Empty transcript")
+        pass
+        load_error, talknet_path, vocoder_path = download_from_drive(
+            self.model_mapping[char], None, RUN_PATH)
+        if load_error is not None:
+            raise ValueError(load_error)
+        with torch.no_grad():
+            if self.tnpath != talknet_path:
+                singer_path = os.path.join(
+                    os.path.dirname(talknet_path), "TalkNetSinger.nemo"
+                )
+                if os.path.exists(singer_path):
+                    print("=== USING SINGER MODEL ===")
+                    tnmodel = TalkNetSingerModel.restore_from(singer_path)
+                else:
+                    tnmodel = TalkNetSpectModel.restore_from(talknet_path)
+                durs_path = os.path.join(
+                    os.path.dirname(talknet_path), "TalkNetDurs.nemo"
+                )
+                pitch_path = os.path.join(
+                    os.path.dirname(talknet_path), "TalkNetPitch.nemo"
+                )
+                if os.path.exists(durs_path):
+                    tndurs = TalkNetDursModel.restore_from(durs_path)
+                    tnmodel.add_module("_durs_model", tndurs)
+                    tnpitch = TalkNetPitchModel.restore_from(pitch_path)
+                    tnmodel.add_module("_pitch_model", tnpitch)
+                else:
+                    tndurs = None
+                    tnpitch = None
+                tnmodel.eval()
+                tnpath = talknet_path
+
+        token_list, tokens, arpa = self.extract_dur.get_tokens(transcript)
+        durs = self.extract_dur.get_duration(wav_name,
+            transcript, token_list)
+
+        wav_f0_info = self.wav_f0s[wav_name]["f0_with_silence"]
+
+        spect = tnmodel.force_spectrogram(
+            tokens=tokens,
+            durs=torch.from_numpy(durs)
+            .view(1, -1)
+            .type(torch.LongTensor)
+            .to(DEVICE),
+            f0=torch.FloatTensor(wav_f0_info).view(1, -1).to(DEVICE),
+        )
+
+        # Vocoding
+        if self.last_voc != vocoder_path:
+            self.voc = vocoder.HiFiGAN(vocoder_path, "config_v1", DEVICE)
+            self.last_voc = vocoder_path
+        audio, audio_torch = self.voc.vocode(spect)
+        # Something here is leaking memory ^^^
+
+        # Super-resolution
+        if self.sr_voc is None:
+            self.sr_voc = vocoder.HiFiGAN(
+                os.path.join(RUN_PATH, "models", "hifisr"), "config_32k", DEVICE
+            )
+        sr_mix, new_rate = self.sr_voc.superres(audio, 22050)
+
+        # Create buffer
+        buffer = io.BytesIO()
+        wavfile.write(buffer, new_rate, sr_mix.astype(np.int16))
+
+        output_name = os.path.basename(wav_name) + "talknet"
+
+        if self.output_mels:
+            mel = spect.to('cpu').squeeze().detach().numpy().transpose()
+            np.save("TalkNet_"+os.path.basename(wav_name)[:-len(".wav")]+".npy",
+                mel)
+
+        return [buffer.getvalue(), arpa, output_name]
+
+ats = AltTalknetServer()
+
+@app.route('/characters', methods=['GET'])
+def get_characters():
+    return jsonify(ats.model_mapping)
+
+@app.route('/upload', methods=['POST'])
+def post_audio():
+    json_data = request.get_json()
+    data, arpa, name = ats.generate_audio(
+        json_data["char"],json_data["wav"],json_data["transcript"])
+    output_wav = os.path.join(json_data["results_dir"],name).with_suffix('.wav')
+    with open(output_wav,'w') as f:
+        f.write(data)
+    return jsonify({"output_path":output_wav,"arpabet":arpa})
+    
+if __name__ == '__main__':
+    app.run(debug=True)
