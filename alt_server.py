47f1e5b6e2a494fcbdb881f4fe1b571e2268f76c
Baked Pineapple <bakedpineapple@airmail.cc>
2023-03-02 01:09:20 -0800

    label dra files with slugified transcript

--- alt_server.py
+++ alt_server.py
@@ -27,6 +27,25 @@ DEVICE = "cuda:0"
 RUN_PATH = os.path.dirname(os.path.realpath(__file__))
 OUTPUT_MELS = True
 
+import unicodedata
+import re
+
+def slugify(value, allow_unicode=False):
+    """
+    Taken from https://github.com/django/django/blob/master/django/utils/text.py
+    Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
+    dashes to single dashes. Remove characters that aren't alphanumerics,
+    underscores, or hyphens. Convert to lowercase. Also strip leading and
+    trailing whitespace, dashes, and underscores.
+    """
+    value = str(value)
+    if allow_unicode:
+        value = unicodedata.normalize('NFKC', value)
+    else:
+        value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+    value = re.sub(r'[^\w\s-]', '', value.lower())
+    return re.sub(r'[-\s]+', '-', value).strip('-_')
+
 # Assumes ref audio, no metallic noise reduction, no autotune
 class AltTalknetServer:
     def __init__(self):
@@ -182,7 +201,10 @@ class AltTalknetServer:
         buffer = io.BytesIO()
         wavfile.write(buffer, new_rate, sr_mix.astype(np.int16))
 
-        output_name = pathlib.Path(os.path.basename(wav_name)).stem + "_"+char+"tn"
+        if disable_reference_audio:
+            output_name = slugify(transcript)[:30]+"_"+char+"tn"
+        else:
+            output_name = pathlib.Path(os.path.basename(wav_name)).stem + "_"+char+"tn"
 
         mel = spect.to('cpu').squeeze().detach().numpy().transpose()
         return [buffer.getvalue(), arpa, output_name, mel]
